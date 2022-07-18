648993edf95df19dd419f9c0e7495f0a22a167e6
Sundar <sun.mng@gmail.com>
2022-07-18 11:38:21 +0800

    IP to domain name changes done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   //baseUrl: 'http://www2.stlogs.com/eSecurityApi/',
-  baseUrl: 'http://10.189.141.32/eSecurityApi/',
+  //baseUrl: 'http://10.189.141.32/eSecurityApi/',
+  baseUrl: 'http://esecurity.stlogs.com/eSecurityApi/',
   headerAuthorization:'stladmin:$tl@dmin'
 };
